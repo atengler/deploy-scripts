fc99fa27ef643f854f3f68d0784ff5260381523c
Adam Tengler <adam.tengler@tcpcloud.eu>
2016-06-17 17:30:30 +0200

    Yum installation variant for minion script

--- bootstrap-minion.sh
+++ bootstrap-minion.sh
@@ -18,8 +18,16 @@ MINION_ID=${MINION_HOSTNAME}.${CONFIG_DOMAIN}
 # FUNCTIONS
 #
 
-install_salt_minion_pkg()
+install_salt_minion_pkg_apt()
 {
+    echo -e "\nPreparing base OS repository ...\n"
+
+    echo -e "deb [arch=amd64] http://apt.tcpcloud.eu/nightly/ trusty main security extra tcp" > /etc/apt/sources.list
+    wget -O - http://apt.tcpcloud.eu/public.gpg | apt-key add -
+
+    apt-get clean
+    apt-get update
+
     echo -e "\nInstalling salt minion ...\n"
 
     if [ "$SALT_VERSION" == "latest" ]; then
@@ -28,15 +36,7 @@ install_salt_minion_pkg()
       apt-get install -y --force-yes salt-common=$SALT_VERSION salt-minion=$SALT_VERSION
     fi
 
-    echo -e "\nPreparing base OS repository ...\n"
-    
-    echo -e "deb [arch=amd64] http://apt.tcpcloud.eu/nightly/ trusty main security extra tcp" > /etc/apt/sources.list
-    wget -O - http://apt.tcpcloud.eu/public.gpg | apt-key add -
-    
-    apt-get clean
-    apt-get update
-
-    echo -e "\nInstalling salt minion ...\n"
+    echo -e "\nConfiguring salt minion ...\n"
     
     [ ! -d /etc/salt/minion.d ] && mkdir -p /etc/salt/minion.d
     echo -e "master: $MINION_MASTER\nid: $MINION_ID" > /etc/salt/minion.d/minion.conf
@@ -46,10 +46,35 @@ install_salt_minion_pkg()
     salt-call pillar.data > /dev/null 2>&1
 }
 
-install_salt_minion_pip()
+install_salt_minion_pkg_yum()
 {
+    echo -e "\nPreparing base OS repository ...\n"
+
+    source /etc/os-release
+    yum install -y https://repo.saltstack.com/yum/redhat/salt-repo-latest-1.el${VERSION_ID}.noarch.rpm
+
+    yum clean all
+
     echo -e "\nInstalling salt minion ...\n"
 
+    if [ "$SALT_VERSION" == "latest" ]; then
+        yum install -y salt-minion
+    else
+        yum install -y salt-minion-$SALT_VERSION
+    fi
+
+    echo -e "\nConfiguring salt minion ...\n"
+
+    [ ! -d /etc/salt/minion.d ] && mkdir -p /etc/salt/minion.d
+    echo -e "master: $MINION_MASTER\nid: $MINION_ID" > /etc/salt/minion.d/minion.conf
+
+    service salt-minion restart
+
+    salt-call pillar.data > /dev/null 2>&1
+}
+
+install_pip_apt()
+{
     echo -e "\nPreparing base OS repository ...\n"
     
     echo -e "deb [arch=amd64] http://apt.tcpcloud.eu/nightly/ trusty main security extra tcp" > /etc/apt/sources.list
@@ -58,14 +83,30 @@ install_salt_minion_pip()
     apt-get clean
     apt-get update
 
-    echo -e "\nInstalling salt minion ...\n"
+    echo -e "\nInstalling pip ...\n"
     
     if [ -x "`which invoke-rc.d 2>/dev/null`" -a -x "/etc/init.d/salt-minion" ] ; then
         apt-get purge -y salt-minion salt-common && apt-get autoremove -y
     fi
     
     apt-get install -y python-pip python-dev zlib1g-dev reclass git
-    
+}
+
+install_pip_yum()
+{
+    echo -e "\nPreparing base OS repository ...\n"
+
+    yum install epel-release
+
+    yum clean all
+
+    echo -e "\nInstalling pip ...\n"
+
+    yum install -y python-pip python-dev zlib1g-dev reclass git
+}
+
+install_salt_minion_pip()
+{ 
     if [ "$SALT_VERSION" == "latest" ]; then
         pip install salt
     else
@@ -74,6 +115,8 @@ install_salt_minion_pip()
 
     [ ! -d /etc/salt/minion.d ] && mkdir -p /etc/salt/minion.d
     echo -e "master: $MINION_MASTER\nid: $MINION_ID" > /etc/salt/minion.d/minion.conf
+
+    # TODO: get CentOS init script variant here
 
     wget -O /etc/init.d/salt-minion https://anonscm.debian.org/cgit/pkg-salt/salt.git/plain/debian/salt-minion.init && chmod 755 /etc/init.d/salt-minion
     ln -s /usr/local/bin/salt-minion /usr/bin/salt-minion
@@ -88,8 +131,23 @@ install_salt_minion_pip()
 #
 
 if [ "$SALT_SOURCE" == "pkg" ]; then
-    install_salt_minion_pkg
+    if [[ $(which yum 2> /dev/null) ]]; then
+        install_salt_minion_pkg_yum
+    elif [[ $(which apt-get 2> /dev/null) ]]; then
+        install_salt_minion_pkg_apt
+    else
+        echo "Unsupported package manager, exiting ..."
+        exit 1
+    fi
 elif [ "$SALT_SOURCE" == "pip" ]; then
+    if [[ $(which yum 2> /dev/null) ]]; then
+        install_pip_yum
+    elif [[ $(which apt-get 2> /dev/null) ]]; then
+        install_pip_apt
+    else
+        echo "Unsupported package manager, exiting ..."
+        exit 1
+    fi
     install_salt_minion_pip
 fi
 
